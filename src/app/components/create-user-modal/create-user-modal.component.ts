0b01752480fc1888bf205d538ecb4b47a546c3fa
yasir gonzalez <calixto.y.gonzalez@gmail.com>
2020-11-05 20:06:37 -0300

    adds captcha

--- src/app/components/create-user-modal/create-user-modal.component.ts
+++ src/app/components/create-user-modal/create-user-modal.component.ts
@@ -5,6 +5,7 @@ import {UsersService} from '../../services/users.service';
 import {AngularFireStorage} from '@angular/fire/storage';
 import {AuthenticationService} from '../../services/authentication.service';
 import * as firebase from 'firebase';
+import {WindowService} from '../../services/window.service';
 
 @Component({
   selector: 'app-create-user-modal',
@@ -19,12 +20,29 @@ export class CreateUserModalComponent implements OnInit {
   userForm: FormGroup;
   firstImage: any;
   secondImage: any;
+  windowRef: any;
+  disableSubmit: boolean;
 
-  constructor(public activeModal: NgbActiveModal, private fb: FormBuilder, private cd: ChangeDetectorRef, private authentication: AuthenticationService) {
+
+  constructor(public activeModal: NgbActiveModal, private fb: FormBuilder, private windowsService: WindowService, private authentication: AuthenticationService) {
   }
 
   ngOnInit(): void {
-    // window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('submit-user-creation');
+    this.disableSubmit = true;
+    setTimeout(() => {
+      this.disableSubmit = false;
+    },5000);
+    this.windowRef = this.windowsService.windowRef;
+    this.windowRef.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('submit-user-creation', {
+      size: 'normal',
+      callback: (response) => {
+        console.log(response);
+        this.submitAfterCaptcha();
+      },
+      'expired-callback': () => {
+        console.log("EXPIRED CAPTCHA")
+      }
+    });
     this.userForm = this.fb.group({
       name: ['', Validators.required],
       lastname: ['', Validators.required],
@@ -57,16 +75,19 @@ export class CreateUserModalComponent implements OnInit {
 
   submit() {
     // this.authentication.sendSignInLinkToEmail(this.userForm.value.email, this.userForm.value.password);
-    // if (this.userForm.valid) {
-    //   this.authentication.createUser(this.userForm.value.email, this.userForm.value.password, {
-    //     ...this.userForm.value,
-    //     firstImage: this.firstImage,
-    //     secondImage: this.secondImage
-    //   }).finally(
-    //     this.activeModal.close
-    //   );
-    // }
+    this.render();
 
+  }
+  submitAfterCaptcha() {
+    if (this.userForm.valid) {
+      this.authentication.createUser(this.userForm.value.email, this.userForm.value.password, {
+        ...this.userForm.value,
+        firstImage: this.firstImage,
+        secondImage: this.secondImage
+      }).finally(
+        this.activeModal.close
+      );
+    }
   }
 
   onFileChange(event: Event, id: string) {
@@ -92,4 +113,14 @@ export class CreateUserModalComponent implements OnInit {
   }
 
 
+  render() {
+    this.windowRef.recaptchaVerifier.render();
+  }
+
+
+  verify() {
+    this.windowRef.recaptchaVerifier.verify().then(value => {
+      console.log(value)
+    });
+  }
 }
